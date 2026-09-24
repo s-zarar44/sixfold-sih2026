dbde3f96517ad7f28e53f3d38d5ffebf0f17ac69
Zarar <zararmajeed100@gmail.com>
2026-09-24 20:47:33 +0530

    Initial project structure

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+   const express = require('express');
+   const app = express();
+
+   app.get('/', (req, res) => res.send('SixFold backend running'));
+
+   app.listen(5000, () => console.log('Server on port 5000'));
